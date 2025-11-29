9388a45606517890ce0810686a944821f0b66ffd
Clic-stack <175146257+Clic-stack@users.noreply.github.com>
2025-11-29 13:30:07 -0600

    Update CORS

--- movies-app-backend/src/env/index.js
+++ movies-app-backend/src/env/index.js
@@ -1,4 +1,5 @@
 export const env = {
     PORT: process.env.PORT,
-    DATABASE_URL: process.env.DATABASE_URL
+    DATABASE_URL: process.env.DATABASE_URL,
+    CORS_ORIGIN: process.env.CORS_ORIGIN
 }
