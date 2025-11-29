2e5ae8576bbdd6fe68d3d849078037e1c74d9fd5
Clic-stack <175146257+Clic-stack@users.noreply.github.com>
2025-11-29 10:50:45 -0600

    Ninth commit: Update axios.js in utils carpet

--- movies-app-frontend/src/utils/axios.js
+++ movies-app-frontend/src/utils/axios.js
@@ -1,5 +1,12 @@
-import axios from "axios";
+/*import axios from "axios";
 
 export default axios.create({
     baseURL: import.meta.env.VITE_API_URL
-})
+})*/
+
+import axios from "axios";
+
+// Usa la variable de entorno si existe, o el localhost como fallback
+const baseURL = import.meta.env.VITE_API_URL || "http://localhost:4000/api";
+
+export default axios.create({ baseURL });
